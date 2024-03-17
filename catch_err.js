efcc5570b31c20f82ebd87ef990f686fbda56e23
trungvuive <trung150502@gmail.com>
2024-03-17 17:56:45 +0700

    update post run code and handle error by display

--- catch_err.js
+++ catch_err.js
@@ -1,13 +1,25 @@
 const spawn = require('spawn-please');
 
-const ls = spawn("cabal run", ["-v0"]);
+const haskell_run_command = spawn("cabal run", ["-v0"]);
+const post_execution_run_command = spawn('API_KEY=NR23lHuWdW6R7zrBe5kqa-mKviTOPPnFF0GQEohuy14HAzFuXTtO-g== node ./mock-server/pre.js')
 
-ls.then(result => {
-		console.log('OUT: ' + result.stdout);
-		console.log('RUN SUCCESS');
+haskell_run_command
+.then(result => {
+	console.log('OUT: ' + result.stdout);
+	console.log('RUN SUCCESS');
+
+	post_execution_run_command
+	.then(result => {
+		console.log('OUT 2: ' + result.stdout);
 	})
 	.catch(err => {
 		console.log('ERR: ' + err.stdout);
 		console.log('ERR: ' + err.stderr);
-		throw new Error('UNEXPECTED ERROR!! - PLEASE CHECK YOUR EXERCISE FORMAT!!');	
-	});
+		throw new Error('UNEXPECTED ERROR!! - PLEASE CHECK YOUR EXERCISE FORMAT!!');
+	})
+})
+.catch(err => {
+	console.log('ERR: ' + err.stdout);
+	console.log('ERR: ' + err.stderr);
+	throw new Error('UNEXPECTED ERROR!! - PLEASE CHECK YOUR EXERCISE FORMAT!!');
+});
