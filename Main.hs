87c72307bd61a32caa959e7d589ebfad68bdfa7c
trungvuive <trung150502@gmail.com>
2024-03-23 21:30:07 +0700

    update new Main refactor

--- Main.hs
+++ Main.hs
@@ -5,68 +5,65 @@ import ToDo
 str_to_int :: String -> Int
 str_to_int str = read str
 
+list_str_to_int :: [String] -> [Int]
+list_str_to_int [] = []
+list_str_to_int (x:xs) = str_to_int x : list_str_to_int xs
+
+
+execute_exercise_0 :: (String, String) -> IO()
+execute_exercise_0 tuple = do
+    {- 
+        This Exercise read input of 2 numbers
+        output a number
+    -}
+    raw_input_0 <- readFile (fst tuple)
+
+    let input_str_list = lines raw_input_0
+
+    let input_list = list_str_to_int input_str_list
+
+    let input0 = input_by_lines !! 0
+    let input1 = input_by_lines !! 1
+
+    let resultStr = show (addNum input0 input1)
+
+    writeFile (snd tuple) resultStr
+
+
+-- execute_exercise_1 :: IO()
+-- execute_exercise_1 = do
+--     {- 
+--         This Exercise read input of List
+--         output a number
+--     -}
+--     raw_input_1 <- readFile "in1test1"
+--     let input_str_list = words raw_input_1
+
+--     let input_list = list_str_to_int input_str_list
+
+--     let resultStr = show (sumList input_list)
+
+--     writeFile "out1" resultStr
+
+
+-- execute_exercise_2 :: IO()
+-- execute_exercise_2 = do
+--     {- 
+--         This Exercise read input of String
+--         output a String
+--     -}
+--     raw_input_2 <- readFile "in2test1"
+--     let input_str_list = words raw_input_2
+
+--     let input = str_to_int (input_str_list !! 0)
+
+--     let resultStr = show (grayCode input)
+
+--     writeFile "out2" outStr3
+
+
 main :: IO ()
 main = do
-    -- TEST EXERCISE 1
-    -- reading input with numbers, output with numbers
-    tests <- readFile "in0test1"
-    let test_lines = lines tests
-
-    -- print test_lines
-    
-    let num1s = head test_lines
-    let num1 = str_to_int num1s
-    let test_lines1 = tail test_lines
-
-    -- print num1
-
-    let num2s = head test_lines1
-    let num2 = str_to_int num2s
-    let test_lines2 = tail test_lines1
-
-    let result1 = addNum num1 num2
-    let outStr = show result1
-    writeFile "out0" outStr
-
-
-
-    -- TEST EXERCISE 2
-    -- reading input with list
-    -- output number
-
-    tests2 <- readFile "in1test1"
-    let num_str_list = words tests2
-
-
-    let num4s = num_str_list !! 0
-    let num4 = str_to_int num4s
-
-
-    let num5s = num_str_list !! 1
-    let num5 = str_to_int num5s
-
-
-    let num6s = num_str_list !! 2
-    let num6 = str_to_int num6s
-
-
-    let list = [num4, num5, num6]
-
-    let result2 = sumList list
-    let outStr2 = show result2
-    writeFile "out1" outStr2
-    
-    
-    -- TEST EXERCISE 3
-    -- reading input string
-    -- output string
-    
-    tests3 <- readFile "in2test1"
-    let num_str_list2 = words tests3
-    
-    let num7s = num_str_list !! 0
-    let num7 = str_to_int num7s
-    
-    let result3 = grayCode num7
-    let outStr3 = show result3
-    writeFile "out2" outStr3
+    execute_exercise_0 ("in0test1", "out0test1")
+    -- execute_exercise_1
+    -- execute_exercise_2
