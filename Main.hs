2125cfa5debd688ce9950d2f31d94de525844db8
trungvuive <trung150502@gmail.com>
2024-03-23 21:51:32 +0700

    fix bug on Main haskell file

--- Main.hs
+++ Main.hs
@@ -18,52 +18,52 @@ execute_exercise_0 tuple = do
     -}
     raw_input_0 <- readFile (fst tuple)
 
-    let input_str_list = lines raw_input_0
+    let input_str_list = words raw_input_0
 
     let input_list = list_str_to_int input_str_list
 
-    let input0 = input_by_lines !! 0
-    let input1 = input_by_lines !! 1
+    let input0 = input_list !! 0
+    let input1 = input_list !! 1
 
     let resultStr = show (addNum input0 input1)
 
     writeFile (snd tuple) resultStr
 
 
--- execute_exercise_1 :: IO()
--- execute_exercise_1 = do
---     {- 
---         This Exercise read input of List
---         output a number
---     -}
---     raw_input_1 <- readFile "in1test1"
---     let input_str_list = words raw_input_1
+execute_exercise_1 :: (String, String) -> IO()
+execute_exercise_1 tuple = do
+    {- 
+        This Exercise read input of List
+        output a number
+    -}
+    raw_input_1 <- readFile (fst tuple)
+    let input_str_list = words raw_input_1
 
---     let input_list = list_str_to_int input_str_list
+    let input_list = list_str_to_int input_str_list
 
---     let resultStr = show (sumList input_list)
+    let resultStr = show (sumList input_list)
 
---     writeFile "out1" resultStr
+    writeFile (snd tuple) resultStr
 
 
--- execute_exercise_2 :: IO()
--- execute_exercise_2 = do
---     {- 
---         This Exercise read input of String
---         output a String
---     -}
---     raw_input_2 <- readFile "in2test1"
---     let input_str_list = words raw_input_2
+execute_exercise_2 :: (String, String) -> IO()
+execute_exercise_2 tuple = do
+    {- 
+        This Exercise read input of String
+        output a String
+    -}
+    raw_input_2 <- readFile (fst tuple)
+    let input_str_list = words raw_input_2
 
---     let input = str_to_int (input_str_list !! 0)
+    let input = str_to_int (input_str_list !! 0)
 
---     let resultStr = show (grayCode input)
+    let resultStr = show (grayCode input)
 
---     writeFile "out2" outStr3
+    writeFile (snd tuple) resultStr
 
 
 main :: IO ()
 main = do
-    execute_exercise_0 ("in0test1", "out0test1")
-    -- execute_exercise_1
+    execute_exercise_0 ("in0", "out0test1")
+    execute_exercise_1 ("in1", "out1test1")
     -- execute_exercise_2
